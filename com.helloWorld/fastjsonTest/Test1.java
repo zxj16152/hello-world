8a5a32f58020d087367dad189ff170b235312268
xinjian.zhou <xinjian.zhou@phicomm.com>
2018-04-27 16:01:48 +0800

    test

---
+++ com.helloWorld/fastjsonTest/Test1.java
@@ -0,0 +1,26 @@
+/*
+package fastjsonTest;
+
+import java.util.List;
+import com.alibaba.fastjson.JSON;
+
+*/
+/**
+ * User: xinjian.zhou
+ * Mail: xinjian.zhou@phicomm.com
+ * Date: 2018/4/24
+ * Time: 16:53
+ * Description:
+ *//*
+
+public class Test1 {
+    public static void main(String[] args){
+        Type type = new TypeReference<List<Model>>() {}.getType();
+        List<Model> list = JSON.parseObject(jsonStr, type);
+        System.out.println("Test1");
+    }
+
+}
+class Model{
+
+}*/
