10670fd692f1149edc86194b3ecc3679663ff05e
xinjian.zhou <xinjian.zhou@phicomm.com>
2018-05-11 18:08:16 +0800

    zxj

---
+++ com.helloWorld/TestExampleTypeList.java
@@ -0,0 +1,22 @@
+/**
+ * User: xinjian.zhou
+ * Mail: xinjian.zhou@phicomm.com
+ * Date: 2018/5/9
+ * Time: 15:22
+ * Description:
+ */
+
+
+/**
+ * 调用示例
+ * @author chenZhiyong
+ * @2018-3-14
+ */
+public class TestExampleTypeList {
+        public static void main(String[] args){
+            String string= "helloworld";
+            String substring =string.substring(string.indexOf("-"), 4);
+
+            System.out.println(substring);
+        }
+}
